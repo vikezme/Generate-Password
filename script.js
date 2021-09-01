beb309b1fe948dae9b26c24450d71c3fe8ae0825
kumar vikrant <vikezme@gmail.com>
2021-09-02 09:24:55 +0930

    Adding file

--- script.js
+++ script.js
@@ -16,39 +16,43 @@ function randomNumberGenerator(min, max) {
 
 function generatePassword() {
 
-  var length = window.prompt('How many letters would you like in your password?')
+  var length = prompt("How many letters would you like in your password?");
 
-  var includeLowercase = window.confirm('Do you want lowercase?')
+  var includeLowercase = confirm("Do you want lowercase?");
 
-  var includeUppercase = window.confirm('Do you want uppercase?')
+   var includeUppercase = confirm("Do you want uppercase?");
 
-  var includeNumbers = window.confirm('Do you want numbers?')
+   var includeNumbers = confirm("Do you want numbers?");
 
-  var includeSpecialcharacters = window.confirm('Do you want special characters?')
+   var includeSpecialcharacters = confirm("Do you want special characters?");
 
   var password = ''
 
 
 
-  if (includeLowercase === true) {
+  if (includeLowercase ) {
     var randomIndex = randomNumberGenerator(0, lowercaseChar.length)
     password += lowercaseChar[randomIndex]
-    
+  
   }
-  if (includeUppercase === true) {
+  if (includeUppercase ) {
     var randomIndex2 = randomNumberGenerator(0, uppercaseChar.length)
     password += uppercaseChar[randomIndex2]
+
   }
-  if (includeNumbers === true) {
+  if (includeNumbers ) {
     var randomIndex3 = randomNumberGenerator(0, numericalChar.length)
     password += numericalChar[randomIndex3]
+
   }
-  if (includeSpecialcharacters === true) {
+  if (includeSpecialcharacters ) {
     var randomIndex4 = randomNumberGenerator(0, specialChar.length)
     password += specialChar[randomIndex4]
+
   }
 
   var rest =length - password.length
+
  for(var i= 0; i< rest; i++) {
      console.log(i)
      var randomIndex5 = randomNumberGenerator(0, allChar.length)
